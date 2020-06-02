cabfad6b6c9ddf88375e4fb525bf2c9683b0dc85
Emil Vaklinov <emil_vaklinov@yahoo.com>
2020-06-02 14:53:53 +0100

    app connected to Parse DB

--- FourSquare/AppDelegate.swift
+++ FourSquare/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Parse
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -15,6 +16,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let configuration = ParseClientConfiguration { (ParseMutableClientConfiguration) in
+            ParseMutableClientConfiguration.applicationId = "3l3QDVQYfkFxenfh8xLzuUb5tCcQtD6zdrrngYrj"
+            ParseMutableClientConfiguration.clientKey = "QDVZTB5Qns6t41CqOKiz4BaJRLU9x3oFWfVPvqCZ"
+            ParseMutableClientConfiguration.server = "https://parseapi.back4app.com/"
+        }
+        
+        Parse.initialize(with: configuration)
+        
         return true
     }
 
