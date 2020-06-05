879763d12533b40d93319d41ebd9a8d2489e9da2
Emil Vaklinov <emil_vaklinov@yahoo.com>
2020-06-05 09:49:23 +0100

    sign in users done

--- FourSquare/ViewController.swift
+++ FourSquare/ViewController.swift
@@ -12,22 +12,42 @@ import Parse
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var usernameText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-        let parseObject = PFObject(className: "Fruits")
-        parseObject["name"] = "Apple"
-        parseObject["calories"] = 100
-        parseObject.saveInBackground { (success, error) in
-            if error != nil {
-                print(error?.localizedDescription)
-            } else {
-                print("Success")
-            }
-        }
+
     }
 
-
+    @IBAction func signInClicked(_ sender: Any) {
+    }
+    
+    @IBAction func signUpClicked(_ sender: Any) {
+        
+        if usernameText.text != "" && passwordText.text != "" {
+            let user = PFUser()
+            user.username = usernameText.text
+            user.password = passwordText.text
+            
+            user.signUpInBackground { (success, error) in
+                if error != nil {
+                    self.makeAlert(titleInput: "Error", messageInput: error?.localizedDescription ?? "Error!!")
+                } else {
+                    // Segue
+                    print("OK!")
+                }
+                
+            }
+                    } else {
+            makeAlert(titleInput: "Error", messageInput: "Username / Password ??")
+        }
+    }
+    
+    func makeAlert(titleInput: String, messageInput: String) {
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
+    }
 }
 
