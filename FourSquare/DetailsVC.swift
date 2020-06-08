beea50b7df77de4503fdfdb49688dc6c09c9dc43
Emil Vaklinov <emil_vaklinov@yahoo.com>
2020-06-08 09:41:35 +0100

    details vc created

---
+++ FourSquare/DetailsVC.swift
@@ -0,0 +1,32 @@
+//
+//  DetailsVC.swift
+//  FourSquare
+//
+//  Created by Emil Vaklinov on 08/06/2020.
+//  Copyright © 2020 Emil Vaklinov. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class DetailsVC: UIViewController {
+
+    @IBOutlet weak var detailsImageView: UIImageView!
+    
+    @IBOutlet weak var detailsNameLabel: UILabel!
+    
+    @IBOutlet weak var detailsTypeLabel: UILabel!
+    
+    @IBOutlet weak var detailsAtmosphereLabel: UILabel!
+    
+    @IBOutlet weak var detailsMapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
